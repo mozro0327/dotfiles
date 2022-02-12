61a1c97e0f0070a8111733e3ade376466c579f18
Cheuk-Wing <mozro.0327@outlook.com>
2022-02-12 23:42:34 +0800

    add other dotfiles

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function proxy
+  set -xg ALL_PROXY http://127.0.0.1:7890
+end
+
+function noproxy
+  set -e ALL_PROXY
+end
+
+starship init fish | source
+
+set -x VISUAL nvim
+set -x EDITOR nvim
